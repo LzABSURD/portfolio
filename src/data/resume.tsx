4dea4ca63fa4d342eebe191cea26316ae3d37dbf
lzabsurd <lz-salazar@hotmail.com>
2024-11-26 23:21:20 -0300

    update: sociais

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -29,31 +29,26 @@ export const DATA = {
     social: {
       GitHub: {
         name: "GitHub",
-        url: "https://dub.sh/dillion-github",
+        url: "https://github.com/LzABSURD",
         icon: Icons.github,
 
         navbar: true,
       },
       LinkedIn: {
         name: "LinkedIn",
-        url: "https://dub.sh/dillion-linkedin",
+        url: "https://www.linkedin.com/in/luansalazar/",
         icon: Icons.linkedin,
 
         navbar: true,
       },
       X: {
         name: "X",
-        url: "https://dub.sh/dillion-twitter",
+        url: "https://x.com/LzAbsurd",
         icon: Icons.x,
 
         navbar: true,
       },
-      Youtube: {
-        name: "Youtube",
-        url: "https://dub.sh/dillion-youtube",
-        icon: Icons.youtube,
-        navbar: true,
-      },
+      
       email: {
         name: "Send Email",
         url: "#",
