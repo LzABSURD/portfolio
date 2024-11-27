42f352bd963f684169333064f0f0fd77bfc9f1c2
lzabsurd <lz-salazar@hotmail.com>
2024-11-26 23:47:40 -0300

    update: contact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -157,21 +157,20 @@ export default function Page() {
           <BlurFade delay={BLUR_FADE_DELAY * 16}>
             <div className="space-y-3">
               <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-                Contact
+              Contato
               </div>
               <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                Get in Touch
+              Entre em Contato
               </h2>
               <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                Want to chat? Just shoot me a dm{" "}
+              Quer conversar? Me mande uma mensagem direta{" "}
                 <Link
-                  href={DATA.contact.social.X.url}
+                  href={DATA.contact.social.LinkedIn.url}
                   className="text-blue-500 hover:underline"
                 >
-                  with a direct question on twitter
+                 com uma pergunta direta no Linkedin 
                 </Link>{" "}
-                and I&apos;ll respond whenever I can. I will ignore all
-                soliciting.
+                e responderei assim que puder.
               </p>
             </div>
           </BlurFade>
