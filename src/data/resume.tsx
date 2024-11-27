3b0938959a1e9301d282dcf41d062b7d1fc3a3ac
lzabsurd <lz-salazar@hotmail.com>
2024-11-26 22:21:25 -0300

    update: text profile section

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,16 +2,16 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "Dillion Verma",
-  initials: "DV",
+  name: "Luan S. Salazar",
+  initials: "Lz",
   url: "https://dillion.io",
-  location: "San Francisco, CA",
+  location: "São José, SC",
   locationLink: "https://www.google.com/maps/place/sanfrancisco",
   description:
-    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
+    "FullStack Developer. Amo criar projetos e soluções que ajudam as pessoas. Florianópolis, SC 🇧🇷",
   summary:
-    "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
-  avatarUrl: "/me.png",
+    "Sou um engenheiro de software em início de carreira, atualmente focado no estudo e desenvolvimento de front-end. Trabalho em uma startup inovadora, onde estou aprendendo e contribuindo para o desenvolvimento de soluções tecnológicas. Tenho interesse em crescer continuamente como profissional, buscando sempre entregar valor rápido e de forma eficiente. Estou confortável com o desenvolvimento de interfaces web e estou aprimorando minhas habilidades em JavaScript, HTML, CSS e frameworks modernos. Motivado e entusiasmado para expandir meus conhecimentos e assumir novos desafios no mundo da tecnologia.",
+  avatarUrl: "/me.jpg",
   skills: [
     "React",
     "Next.js",
